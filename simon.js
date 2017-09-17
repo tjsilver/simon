631d81f2e91ed907b4a110062659d48665c1e2e5
pixelComet <thalia@pyrosomida.co.uk>
2017-09-17 17:00:40 +0100

    timer tweaks, still not working

--- simon.js
+++ simon.js
@@ -1,70 +1,66 @@
 document.addEventListener("DOMContentLoaded", function () {
 
   /* buttons */
+  var start = document.getElementById('start'),
+  strict = document.getElementById('strict'),
+  strictIndicator = document.getElementById('strict-indicator'), strictOn = false;
 
-  var start = document.getElementById('start');
-  var strict = document.getElementById('strict')
-  var strictIndicator = document.getElementById('strict-indicator');
-  var strictOn = false;
-  var timeInterval = 1000;
+  /* global variables */
+  var timeInterval = 1500;
   const WEDGES = ['green', 'red', 'blue', 'yellow'];
 
+  /* set listeners */
   start.addEventListener('click', startPress, false);
   strict.addEventListener('click', useStrict, false);
   for (let i = 0; i < WEDGES.length; i++) {
     document.getElementById(WEDGES[i]).addEventListener('click', wedgeClick, false);
   }
-
+  /* button presses */
   function buttonDown(el) {
     el.classList.add('pressed');
   }
-
   function buttonUp(el) {
     el.classList.remove('pressed');
   }
-
+  /* strict mode */
+  function useStrict() {
+    if (!strictOn) {
+      buttonDown(strict);
+      strictIndicator.classList.add('on');
+      strictOn = true;
+    } else {
+      buttonUp(strict);
+      strictIndicator.classList.remove('on');
+      strictOn = false;
+    }
+    console.log("strict status", strictOn);
+  }
+  /* start game */
   function startPress() {
-    console.log("*******startPress with this.id", this.id);
-    var id = this.id;
-    var startButton = this;
-    buttonDown(startButton);
+    console.log("*******startPress");
+    buttonDown(start);
     //timer then buttonup
     setTimeout(function () {
-      buttonUp(startButton);
+      buttonUp(start);
     }, 500);
     if (game.reset()) {
       game.start();
     }
   }
-
-  function useStrict() {
-    var strictButton = this;
-    if (!strictOn) {
-      buttonDown(strictButton);
-      strictIndicator.classList.add('on');
-      strictOn = true;
-    } else {
-      buttonUp(strictButton);
-      strictIndicator.classList.remove('on');
-      strictOn = false;
-    }
-  }
-
-  function indicatorOff() {
-    strictIndicator.classList.remove('on');
-  }
-
+  /* game */
   var game = {
     gameStarted: false,
     playerTurn: false,
     gameArr: [],
     playerArr: [],
-    count: 0,
-    timeListen: this.timeInterval * this.count,
+    numSteps: 0,
+    playerMoves: 0,
+    timeListen: timeInterval * this.numSteps,
     reset() {
       console.log("Game reset");
-      this.gameArr = [];
-      this.count = 0;
+      game.gameArr = [];
+      game.playerArr = [];
+      game.numSteps = 0;
       return true;
     },
     random() {
@@ -85,20 +81,20 @@ document.addEventListener("DOMContentLoaded", function () {
     start() {
       console.log("&&&&&&&& game.start()");
       this.gameStarted = true;
-      this.aiTurn();
+      setTimeout(game.aiTurn, timeInterval);
     },
-    fillArray() {
-      console.log("*********game.fillArray()")
-      if (this.count >= 0 && this.count < 20) {
-        var rand = this.random();
-        console.log("rand from fillArray", rand);
-        this.gameArr.push(WEDGES[rand]);
-        this.count++;
-        console.log("count", this.count);
-        console.log("gameArr length", this.gameArr.length);
-        console.log("gameArr", this.gameArr);
+    addToSequence() {
+      console.log("*********game.addToSequence()")
+      if (game.numSteps >= 0 && game.numSteps < 20) {
+        var rand = game.random();
+        console.log("rand from addToSequence", rand);
+        game.gameArr.push(WEDGES[rand]);
+        game.numSteps++;
+        console.log("numSteps", game.numSteps);
+        console.log("gameArr length", game.gameArr.length);
+        console.log("gameArr", game.gameArr);
       }
-      this.displayMoves();
+      game.displayMoves();
     },
     displayMoves() {
       var i = 0;
@@ -119,53 +115,78 @@ document.addEventListener("DOMContentLoaded", function () {
       game.playerTurn = true;
       console.log("game.playerTurn:", game.playerTurn);
       game.playerArr = [];
+      startTiming();
+    },
+    playerOff() {
+      console.log("playerOff!");
+      game.playerTurn = false;
+      console.log('game.playerTurn:', game.playerTurn);
     },
     aiTurn() {
-      console.log("******* game.AiTurn()")
-      game.fillArray();
+      console.log("******* game.AiTurn()");
+      game.addToSequence();
     },
     makePlayerMove(colour) {
-      console.log("******makePlayerMove(),count:", game.count);
-      //new code
-      var playerCount = game.count - 1;
-      console.log('playerCount', playerCount);
-      game.compare(playerCount, colour)
-
+      console.log("******makePlayerMove(),numSteps:", game.numSteps);
+      game.playerArr.push(colour)
+      console.log('game.playerArr:',game.playerArr)
+      game.playerMoves++;
+      console.log('game.playerMoves:', game.playerMoves);
+      if (game.playerMoves == game.numSteps) {
+        game.compare();
+      }
     },
     //new compare
-    compare(pCount, col) {
-      console.log('******* new compare()')
-      if (game.gameArr[pCount] == col) {
-        console.log("same");
-        if (pCount + 1 == game.count) {
-          console.log('end of playerTurn')
-          game.playerTurn = false;
-          game.fillArray();
-        }
-      } else {
-        console.log("different");
-        console.log("start again");
-        game.playerTurn = false;
-        if (strictOn) {
-          game.reset();
-          game.start();
+    compare() {
+      console.log('*******compare()')
+      for (let i=0; i<game.numSteps; i++) {
+        if (game.gameArr[i] == game.playerArr[i]) {
+          console.log('i:',i, 'is same');
         } else {
-          console.log('replay moves');
-          // REPLAY MOVES
+          console.log("i", i, "is different");
+          console.log("start again");
+          game.playerTurn = false;
+          if (strictOn) {
+            game.reset();
+            game.start();
+          } else {
+            console.log('replay moves');
+            // REPLAY MOVES
+            setTimeout(game.displayMoves, timeInterval);
+          }
+          return;
         }
       }
+      game.aiTurn();
     }
+  }
 
+  function startTiming() {
+    console.log('-------startTiming()');
+    // TODO: MAKE THIS WORK WITH ranOutOfTime!!
+  }
+
+  var timer;
+  function ranOutOfTime() {
+    clearTimeout(timer)
+    timer = setTimeout(function() {
+      if (game.playerArr.length < game.gameArr.length) {
+        console.log("ranOutOfTime returning true");
+        return true;
+      }
+      console.log("ranOutOfTime returning false");
+      return false;
+    }, game.timeListen);
   }
 
   function wedgeClick(e) {
     var w = e.target.id;
     console.log("wedgeClick():", w, 'was clicked');
     console.log("game.playerTurn is ", game.playerTurn)
-    if (game.playerTurn == true && game.playerArr.length < game.count) {
-      game.makePlayerMove(w)
+    if (game.playerTurn == true && game.playerArr.length < game.numSteps) {
+      game.makePlayerMove(w);
     } else {
-      console.log('now turn for AI again')
+      console.log('now turn for AI again');
     }
   }
 
@@ -196,7 +217,7 @@ compare() {
 game.playerArr.push(colour);
 console.log('playerArr', game.playerArr)
 game.lightUp(colour);
-if (game.playerArr.length == game.count) {
+if (game.playerArr.length == game.numSteps) {
   game.playerTurn = false;
   console.log('no longer player turn')
   game.compare();
